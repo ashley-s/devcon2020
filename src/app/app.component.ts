e6dfa878c1c44a7e44e0aa183005ca1f4fe48ab0
Ashley Shookhye <ashley.shookhye@gmail.com>
2020-09-09 09:34:59 +0400

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
-import { Observer, Observable } from 'rxjs';
+import { FormControl, Validators, FormGroup, NgForm, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +7,33 @@ import { Observer, Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  name: FormControl;
-  nameValue: string;
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    //standard way of creating a formControl
-    this.name = new FormControl("", [Validators.required, Validators.minLength(5)]);
-    //another way of creating a formControl or a formGroup instance for complex forms
-    // this.name = this.fb.control("", [Validators.required, Validators.minLength(5)]);
+  mainForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    
   }
 
+  ngOnInit() {
+    this.mainForm = this.fb.group({
+      fname: ["", [Validators.required, Validators.minLength(5)]],
+      lname: ["", [Validators.required]]
+    })
+  }
+
+  get fname() {
+    return this.mainForm.get('fname');
+  }
+
+  get lname() {
+    return this.mainForm.get('lname');
+  }
+
+  submitForm(form: NgForm) {
+    console.log(form.valid);
+    console.log('-----------');
+    console.log(form.status);
+    console.log('-----------');
+    console.log('-----------');
+    console.log(form.value);
+    console.log('-----------');
+  }
 }
