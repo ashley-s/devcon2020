4fe725d1ad6cb792af1192a2bb984f1f27a2203e
Ashley Shookhye <ashley.shookhye@gmail.com>
2020-09-09 09:58:51 +0400

    test5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup, NgForm, FormBuilder } from '@angular/forms';
+import { FormControl, Validators, FormGroup, NgForm, FormBuilder, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +9,17 @@ import { FormControl, Validators, FormGroup, NgForm, FormBuilder } from '@angula
 export class AppComponent implements OnInit {
   mainForm: FormGroup;
   constructor(private fb: FormBuilder) {
-    
+
   }
 
   ngOnInit() {
     this.mainForm = this.fb.group({
       fname: ["", [Validators.required, Validators.minLength(5)]],
-      lname: ["", [Validators.required]]
+      lname: ["", [Validators.required]],
+      address: ["", [Validators.required]],
+      phone: ["", [phoneValidatorFactory('mobile')]]
     })
+    //this.onChanges();
   }
 
   get fname() {
@@ -27,6 +30,14 @@ export class AppComponent implements OnInit {
     return this.mainForm.get('lname');
   }
 
+  get address() {
+    return this.mainForm.get('address');
+  }
+
+  get phone() {
+    return this.mainForm.get('phone');
+  }
+
   submitForm(form: NgForm) {
     console.log(form.valid);
     console.log('-----------');
@@ -35,5 +46,56 @@ export class AppComponent implements OnInit {
     console.log('-----------');
     console.log(form.value);
     console.log('-----------');
+  }
+}
+
+/**
+ * The function's definition is take an abstract control as an argument and 
+ * return an object of type ValidationErrors or null
+ * @param control abstract control as parameter
+ */
+export function validateMobilePhone(control: FormControl): ValidationErrors | null {
+  if (control.value && control.value.length != 8) {
+    return {
+      message: "Mobile Phone Number should have 8 numbers"
+    }
+  }
+  if (control.value && control.value.substr(0, 1) != "5") {
+    return {
+      message: "Mobile Phone Number should start with 5"
+    }
+  }
+  return null;
+}
+
+/**
+ * a factory function that return a function
+ * @param type 
+ */
+export function phoneValidatorFactory(type: string) {
+  return function(control: AbstractControl): ValidationErrors | null {
+    if (type == 'mobile') {
+      if (control.value && control.value.length != 8) {
+        return {
+          message: "Mobile Phone Number should have 8 numbers"
+        }
+      }
+      if (control.value && control.value.substr(0, 1) != "5") {
+        return {
+          message: "Mobile Phone Number should start with 5"
+        }
+      }
+    } else if (type == 'fixedline') {
+      if (control.value && control.value.length != 7) {
+        return {
+          message: "Fixed line Number should have 7 numbers"
+        }
+      }
+      if (control.value && control.value.substr(0, 1) != "6") {
+        return {
+          message: "Fixed line Number should start with 6"
+        }
+      }
+    }
   }
 }
